1f2ce45cb6758346ff8b81ccb02e606bd123c698
ShenQi1996 <qisam1989@gmail.com>
2021-09-16 15:33:37 -0400

    Longest Repeating Character Replacement

--- Test.js
+++ Test.js
@@ -221,20 +221,39 @@ window.addEventListener("load", () => {
   //   return layers;
   // };
   //Maximum Depth of Binary Tree
-
-  var maxDepth = function (root) {
-    if (root == null) return 0;
-
-    //travel to the end of all branches in the tree, therby end up with 0 for depth
-    //check left side of node
-    let leftMax = maxDepth(root.left);
-    //check right side node
-    let rightMax = maxDepth(root.right);
-
-    //as call stack updates to the first function call, and gets closer to head node, depth increments by 1
-    //Math max is to update the highest depth from either side of the node
-    let depth = Math.max(leftMax, rightMax);
-
-    return depth + 1;
-  };
+  // var maxDepth = function (root) {
+  //   if (root == null) return 0;
+  //   //travel to the end of all branches in the tree, therby end up with 0 for depth
+  //   //check left side of node
+  //   let leftMax = maxDepth(root.left);
+  //   //check right side node
+  //   let rightMax = maxDepth(root.right);
+  //   //as call stack updates to the first function call, and gets closer to head node, depth increments by 1
+  //   //Math max is to update the highest depth from either side of the node
+  //   let depth = Math.max(leftMax, rightMax);
+  //   return depth + 1;
+  // };
+  //Longest Repeating Character Replacement
+  //   var characterReplacement = function(s, k) {
+  //     const n = s.length;
+  //     const charCount = new Array(26);
+  //     charCount.fill(0);
+  //     let windowStart = 0;
+  //     let maxLength = Number.MIN_VALUE;
+  //     let maxCount = 0;
+  //     for (let windowEnd = 0; windowEnd < n; windowEnd++) {
+  //         const currentCharIdx = s.charCodeAt(windowEnd)-65;
+  //         charCount[currentCharIdx]++;
+  //         maxCount = Math.max(maxCount, charCount[currentCharIdx]);
+  //         // num of chars we need to change < k
+  //         // length-maxCount = num Operations
+  //         while ((windowEnd-windowStart+1)-maxCount > k) {
+  //             const leftChar = s.charCodeAt(windowStart)-65;
+  //             charCount[leftChar]--;
+  //             windowStart++;
+  //         }
+  //         maxLength = (maxLength, windowEnd-windowStart+1);
+  //     }
+  //     return maxLength;
+  // };
 });
