8aa3da2e9cae0c7bb1df916e5173e2e99d0187eb
ShenQi1996 <qisam1989@gmail.com>
2021-09-20 15:42:48 -0400

    Linked List Cycle

--- Test.js
+++ Test.js
@@ -256,4 +256,17 @@ window.addEventListener("load", () => {
   //     }
   //     return maxLength;
   // };
+  //   Linked List Cycle
+  //   var hasCycle = function(head) {
+  //     let slow = head;
+  //     let fast = head;
+  //     while(fast && fast.next){
+  //         slow = slow.next
+  //         fast = fast.next.next
+  //         if(slow === fast){
+  //             return true
+  //         }
+  //     }
+  //     return false
+  // };
 });
